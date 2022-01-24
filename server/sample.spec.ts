e91d2b44ddcacaadd159903a4042a5aa560e6386
David Gálvez <davidgalvezgomez01@gmail.com>
2022-01-24 19:40:52 +0100

    Add sonar-project.properties file

---
+++ server/sample.spec.ts
@@ -0,0 +1,7 @@
+describe("Given a sample test", () => {
+  describe("When it receives a true", () => {
+    test("Then it should be true", async () => {
+      expect(true).toBe(true);
+    });
+  });
+});
