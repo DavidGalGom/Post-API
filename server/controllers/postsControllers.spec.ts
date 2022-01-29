f791a208e5e83e5b4841ea5d4e5e3bebed40a261
David Gálvez <davidgalvezgomez01@gmail.com>
2022-01-29 12:10:17 +0100

    Added test for addPost endpoint

--- server/controllers/postsControllers.spec.ts
+++ server/controllers/postsControllers.spec.ts
@@ -1,8 +1,16 @@
 import Post from "../../database/models/post";
 import TestError from "../../interfaces/testError";
-import { getPostsList } from "./postsControllers";
+import { addPost, getPostsList } from "./postsControllers";
+import IResponseTest from "../../interfaces/response";
 
 jest.mock("../../database/models/post");
+const mockResponse = () => {
+  const res: IResponseTest = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn().mockReturnThis(),
+  };
+  return res;
+};
 
 describe("Given a getPostsList function", () => {
   describe("When it receives an object res", () => {
@@ -44,6 +52,39 @@ describe("Given a getPostsList function", () => {
         "Can't find the posts"
       );
       expect(next.mock.calls[0][0]).toHaveProperty("code", 400);
+    });
+  });
+});
+
+describe("Given an addPost function", () => {
+  describe("When it receives a resolved value", () => {
+    test("Then it should create a new post with a code 201", async () => {
+      const req = {
+        body: {
+          title: "post title",
+          body: "body of the post",
+        },
+      };
+      const res = mockResponse();
+      const newPost = req.body;
+      Post.create = jest.fn().mockResolvedValue(newPost);
+
+      await addPost(req, res, null);
+
+      expect(res.json).toHaveBeenCalledWith(newPost);
+    });
+  });
+
+  describe("When it receives a rejected promise", () => {
+    test("Then it should summon next with an error", async () => {
+      const req = jest.fn();
+      Post.create = jest.fn().mockRejectedValue({});
+      const next = jest.fn();
+      const res = mockResponse();
+
+      await addPost(req, res, next);
+
+      expect(next).toHaveBeenCalled();
     });
   });
 });
